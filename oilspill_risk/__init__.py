e11d2aa73c2856ebdcc116c5a471b6adefbc2733
Bruno Colavitto <110279409+brunotectonico@users.noreply.github.com>
2026-04-28 15:04:09 +0200

    Add OSCAR downloader and seasonal/raster hotspot outputs

---
+++ oilspill_risk/__init__.py
@@ -0,0 +1,37 @@
+"""Reusable modules for coastal oil-spill risk screening."""
+
+from .hotspots import HotspotConfig, RunOptions, run_hotspot_extraction
+from .oscar import (
+    OscarDownloadConfig,
+    StudyArea,
+    download_oscar_for_periods,
+    download_oscar_subset,
+    infer_study_area_from_hotspots,
+    seasonal_periods,
+)
+from .trajectory import (
+    CoastalRiskResult,
+    CurrentField,
+    HotspotSource,
+    SimulationConfig,
+    estimate_coastal_risk,
+    simulate_particles,
+)
+
+__all__ = [
+    "CoastalRiskResult",
+    "CurrentField",
+    "HotspotConfig",
+    "HotspotSource",
+    "OscarDownloadConfig",
+    "RunOptions",
+    "SimulationConfig",
+    "StudyArea",
+    "download_oscar_for_periods",
+    "download_oscar_subset",
+    "estimate_coastal_risk",
+    "infer_study_area_from_hotspots",
+    "run_hotspot_extraction",
+    "seasonal_periods",
+    "simulate_particles",
+]
